6c8438f0cac537edba393a56400cec8cc7004e1e
BERTIN Loïc <bil995o@gmail.com>
2019-06-05 09:07:01 +0200

    #12 test MQTT android

--- PS6WaitingQueue/app/src/main/java/com/example/ps6waitingqueue/activities/LoginActivity.java
+++ PS6WaitingQueue/app/src/main/java/com/example/ps6waitingqueue/activities/LoginActivity.java
@@ -1,8 +1,10 @@
 package com.example.ps6waitingqueue.activities;
 
 import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,14 @@ import com.example.ps6waitingqueue.listener.UsersListener;
 import com.example.ps6waitingqueue.models.User;
 import com.example.ps6waitingqueue.services.UsersService;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity implements UsersListener {
@@ -20,7 +30,10 @@ public class LoginActivity extends AppCompatActivity implements UsersListener {
     Button loginButton;
     EditText usernameEditText;
     EditText passwordEditText;
+    Button testConnectMqtt;
     public static ArrayList<User> usersList;
+
+    private final String url = "tcp://127.0.0.1:1883";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +45,47 @@ public class LoginActivity extends AppCompatActivity implements UsersListener {
         loginButton = findViewById(R.id.loginButton);
         usernameEditText = findViewById(R.id.usernameEditText);
         passwordEditText = findViewById(R.id.password_input);
+        testConnectMqtt = findViewById(R.id.testButtonConnect);
+        testConnectMqtt.setOnClickListener(v-> {
+            mqttConnect();
+        });
+    }
+
+    public void mqttConnect(){
+        String clientId = MqttClient.generateClientId();
+        MqttAndroidClient client =
+                new MqttAndroidClient(this.getApplicationContext(), url,
+                        clientId);
+
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    String payload = "the payload";
+                    byte[] encodedPayload = new byte[0];
+                    try {
+                        encodedPayload = payload.getBytes("UTF-8");
+                        MqttMessage message = new MqttMessage(encodedPayload);
+                        client.publish("currentAppointment",message);
+                        Log.d("Mqtt", "onSuccess");
+                    } catch (UnsupportedEncodingException | MqttException e){
+                        System.err.println(e);
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.d("Mqtt", "onFailure");
+
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void onClickLoginButton(View v) {
