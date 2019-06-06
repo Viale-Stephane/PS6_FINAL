f0157ef3e1fac50a2dd8e2f3aaba3ca1c407fe6a
Viale-Stephane <34646444+Viale-Stephane@users.noreply.github.com>
2019-06-06 14:39:03 +0200

    update back

--- back-end/app/api/user/index.js
+++ back-end/app/api/user/index.js
@@ -5,6 +5,19 @@ const router = new Router();
 
 router.get('/', (req, res) => res.status(200).json(User.get()));
 
+router.get('/:userID', (req, res) => {
+  try {
+    const user = User.getById(req.params.userID);
+    res.status(200).json(user);
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      res.status(400).json(err.extra);
+    } else {
+      res.status(500).json(err);
+    }
+  }
+});
+
 router.post('/', (req, res) => {
   try {
     const user = User.create(req.body);
